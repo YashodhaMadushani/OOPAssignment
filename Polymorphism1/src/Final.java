973d1e3485dd0b732bee8adfae49d6c83f456ce9
Yashodha Madushani <140042845+YashodhaMadushani@users.noreply.github.com>
2023-10-05 18:45:35 +0530

    Add files via upload

---
+++ Polymorphism1/src/Final.java
@@ -0,0 +1,8 @@
+public class Final {
+    public static void main(String[] args) {
+        Vehicle car = new Car();
+        Vehicle motorcycle = new Bike();
+        car.start();
+        motorcycle.start();
+    }
+}
